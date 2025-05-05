24219e379b35aa3955e1c8177fbadb2e609d9fab
nwanaa <winituuu@gmail.com>
2025-05-04 18:18:12 -0800

    membuat tabel di migration

--- database/migrations/2025_05_04_163949_create_ukm_table.php
+++ database/migrations/2025_05_04_163949_create_ukm_table.php
@@ -12,12 +12,11 @@ return new class extends Migration
          Schema::create('ukm', function (Blueprint $table) {
         $table->id();
         $table->string('nama_ukm');
-        $table->text('deskripsi')->nullable(); //kolom di database bisa tidak diisi atau nilainya null
         $table->string('logo')->nullable();
         $table->integer('pengurus_id'); 
         $table->timestamps();
 
-        $table->foreign('pengurus_id')->references('id')->on('user')->onDelete('cascade'); // untuk menghapus otomatis
+        $table->foreign('pengurus_id')->references('id')->on('user')->onDelete('cascade'); 
     });
     }
 
@@ -26,7 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('ukm'); //digunakan untuk menghapus tabel ukm jika tabel tersebut ada, 
-        //agar tidak menyebabkan error saat rollback atau saat setup ulang database.
+        Schema::dropIfExists('ukm'); 
     }
 };
