a4b380bcb6890077fd201eade5a37adb97e676c0
nwanaa <winituuu@gmail.com>
2025-05-25 11:17:52 -0800

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,37 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\KegiatanController;
+use App\Http\Controllers\UKMController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//dashboard
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+//login dan register
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+
+Route::get('/register', function () {
+    return view('auth.register');
+})->name('register');
+
+//ukm
+Route::resource('ukm', UKMController::class);
+Route::resource('kegiatan', KegiatanController::class);
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
